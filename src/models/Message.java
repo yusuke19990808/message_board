e259b188654c655a624f2039f5869888d89b5afb
yusuke.nakamura8 <yn1930221@gmail.com>
2023-03-05 11:56:15 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "messages")
 public class Message {
     @Id
@@ -17,17 +25,17 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="title", length = 255, nullable = false)
+    @Column(name = "title", length = 255, nullable = false)
     private String title;
 
-    @Column(name="content", length = 255, nullable=false)
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
-    @Column(name="created_at", nullable=false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name="update_at", nullable=false)
-    private Timestamp update_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -45,6 +53,14 @@ public class Message {
         this.title = title;
     }
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public Timestamp getCreated_at() {
         return created_at;
     }
@@ -53,13 +69,11 @@ public class Message {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
-
-
 }
--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,18 +5,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PRESISTENCE_UNIT_NAME = "message_board";
+    private static final String PERSISTENCE_UNIT_NAME = "message_board";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
         return getEntityManagerFactory().createEntityManager();
-
     }
 
     private static EntityManagerFactory getEntityManagerFactory() {
         if(emf == null) {
-            emf = Persistence.createEntityManagerFactory(PRESISTENCE_UNIT_NAME);
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
+
         return emf;
     }
 }
