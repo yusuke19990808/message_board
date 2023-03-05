4c84a69cedfaeec65e0f6be0f09c7919da62cd80
yusuke.nakamura8 <yn1930221@gmail.com>
2023-03-05 11:32:13 +0900

    データベース関連のファイルを追加

---
+++ src/models/Message.java
@@ -0,0 +1,65 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name="content", length = 255, nullable=false)
+    private String content;
+
+    @Column(name="created_at", nullable=false)
+    private Timestamp created_at;
+
+    @Column(name="update_at", nullable=false)
+    private Timestamp update_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Timestamp update_at) {
+        this.update_at = update_at;
+    }
+
+
+}
